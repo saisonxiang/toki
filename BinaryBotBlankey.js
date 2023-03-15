06ffe16d73007d7734aa4961c62aca3e658c3c82
Ike <127761735+saisonxiang@users.noreply.github.com>
2023-03-15 12:48:45 -0400

    Add files via upload

---
+++ BinaryBotBlankey.js
@@ -0,0 +1,41 @@
+
+
+
+const fs = require('fs');
+const { Client, Collection, GatewayIntentBits, Partials, EmbedBuilder, Permissions } = require('discord.js');
+const { token } = require('./configtoki.json');
+
+
+const { Guilds, GuildMembers, MessageContent, GuildMessages } = GatewayIntentBits;
+
+
+const { User, Message, Channel, GuildMember } = Partials;
+
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
+  partials: [User, Channel, Message, GuildMember],
+});
+
+const randombase2 = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+const sendRandomMsgbase2 = () => {
+
+ client.channels.cache.get('1032850508223889419').send(randomMsgbase2[randombase2(0, randomMsgbase2.length - 1)]);
+}
+
+setInterval(function () { sendRandomMsgbase2() }, 1000 * 10 * 2 );
+
+const randomMsgbase2 = ['0', '1']
+
+client.on('messageCreate', (Message) => {
+  if (Message.channel.id == "1032850508223889419") {
+      fs.appendFile('ヽ(ﾟ´Д)ﾉﾟbase2.txt', Message.content, (err) => {
+          if (err) throw err;
+      });
+  }
+})
+
+console.log('༼ง ◉ _ ◉༽ง');
+client.login(token);
